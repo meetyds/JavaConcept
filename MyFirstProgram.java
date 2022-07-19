8f048fed81297102c25c033ce51bc219645c4609
Meet Patel <meet.yds@gmail.com>
2022-07-19 20:54:55 +0100

    my first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,10 @@
+package JavaConcept;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Automation Testing");
+        System.out.println("My First Automation Program");
+
+    }
+}
